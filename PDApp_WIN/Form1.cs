f12d887930f65fe8f38c931d2fc127871fccb5ea
nunonogueir444 <nunonogueir444@gmail.com>
2025-02-15 12:24:30 +0000

    update

--- PDApp_WIN/Form1.cs
+++ PDApp_WIN/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,5 +17,33 @@ namespace PDApp_WIN
         {
             InitializeComponent();
         }
+
+    //#############################################################################################
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string arg1 = textBox1.Text;
+            string arg2 = textBox2.Text;
+
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                FileName = "G:\\GitHub_nunonogueir444\\PDApp_TERM\\PDApp_TERM\\bin\\Debug\\PDApp_TERM.exe",
+                Arguments = $"{arg1} {arg2}",
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            using (Process process = Process.Start(startInfo))
+            {
+                using (System.IO.StreamReader reader = process.StandardOutput)
+                {
+                    string result = reader.ReadToEnd();
+                    label1.Text = result;
+                }
+            }
+        }
+
+    //#############################################################################################
     }
 }
